0fcb13e38204866bcbdac60e0a5dc3e2395fb859
jacekbla302 <jacekbla302@gmail.com>
2020-07-07 20:50:20 +0200

    basic ball control

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private const float _MAX_FORCE = 400.0f;
+    private const float _FORCE_INCREMENT = 40.0f;
+
+    private Rigidbody2D _rigidbody;
+    private float _force;
+    private bool _ballThrown = false;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && _force <= _MAX_FORCE)
+        {
+            _force += _FORCE_INCREMENT * Time.deltaTime;
+            Debug.Log(_force);
+        }
+
+        if ((Input.GetKeyUp(KeyCode.Space) || _force > _MAX_FORCE) && !_ballThrown)
+        {
+            _rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+            _rigidbody.AddForce(new Vector2(_force, _force));
+            _ballThrown = true;
+        }
+    }
+}
